3ea470874d2b20dada16297b968702075e569e2a
Nirbhay Singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-10-12 17:58:43 +0530

    hero section animation completed
    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div>
+      <div className="flex flex-col justify-center items-center">
         <motion.h1
           initial={{
             opacity: 0,
@@ -18,8 +18,24 @@ export default function Home() {
           transition={{ duration: 0.33 }}
           className="text-4xl sm:text-6xl font-bold"
         >
-          StableMax
+          Artmind
         </motion.h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.33, delay: 0.35 }}
+          className="text-center text-white/50"
+        >
+          Generate stunning images form text using AI model
+        </motion.p>
       </div>
     </div>
   );
