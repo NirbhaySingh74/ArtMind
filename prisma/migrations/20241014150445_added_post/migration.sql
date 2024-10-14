379959e596d49983771a3800c0af79ae984adf1a
Nirbhay Singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-10-14 20:39:16 +0530

    added post schema

---
+++ prisma/migrations/20241014150445_added_post/migration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "sessions" DROP CONSTRAINT "sessions_user_id_fkey";
+
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "promt" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "seed" INTEGER NOT NULL,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,10 +3,8 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -16,30 +14,29 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  prompt: z
+    .string()
+    .min(7, { message: "Prompt must be atleast 7 character long" }),
 });
 
 export default function Pag() {
   const [outputImg, setOutputImg] = useState<string | null>(null);
-  const [inputPrompt, setInputPrompt] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      prompt: "",
     },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
   return (
     <div className="w-full p-3 h-dvh flex justify-start items-center pt-[72px] flex-col">
-      <div className="w-full border border-red-500 p-3">
+      <div className="w-full p-3">
         <h1 className="text-center font-bold text-white text-4xl">Create</h1>
         <p className="text-white/60 text-center">
           Generate Stunning Images from Text for FREE
@@ -50,25 +47,26 @@ export default function Pag() {
           <p className="text-left text-sm text-white/80">
             Type your prompt below to create any image you can imagine!
           </p>
-          <div className="flex gap-2 w-full">
+          <div className="flex gap-2 w-full ">
             <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="">
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full flex gap-2"
+              >
                 <FormField
                   control={form.control}
-                  name="username"
+                  name="prompt"
                   render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
+                    <FormItem className="w-full max-w-[70%]">
+                      {/* <FormLabel>Username</FormLabel> */}
                       <FormControl>
                         <Input
                           placeholder="a cat sitting over a sofa..."
-                          className="max-w-[70%] transition-all"
+                          className="w-full text-white/60 transition-all border-white"
                           {...field}
                         />
                       </FormControl>
-                      <FormDescription>
-                        This is your public display name.
-                      </FormDescription>
+
                       <FormMessage />
                     </FormItem>
                   )}
