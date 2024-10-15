31ae816b8ad29d7874a8db9bbd91438da0f4ee1d
Nirbhay Singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-10-15 13:46:32 +0530

    added image api

---
+++ src/app/api/image/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { prompt } = await request.json();
+
+  function generateRandomNumber(): number {
+    return Math.floor(Math.random() * 10000000) + 1;
+  }
+
+  const randomSeed = generateRandomNumber();
+  const imageURL = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+    prompt
+  )}&seed=${randomSeed}`;
+
+  await fetch(imageURL);
+  return NextResponse.json({ url: imageURL });
+}
